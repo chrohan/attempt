2be3a61ed4e2cca82ce58124392fc7c169effbbf
chrohan <chrohan007@gmail.com>
2021-10-14 08:25:55 +0530

    till if-else

---
+++ HelloWorld.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+
+int quantity;
+printf("quantities  ");
+scanf("%d",&quantity);
+int price;
+printf("price per quantity  ");
+scanf("%d",&price );
+
+int total= quantity*price;
+printf("%d\n",total );
+
+ if (total>1000)
+ {
+   float final= total-(0.1*total);
+printf("%f\n",final );
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
