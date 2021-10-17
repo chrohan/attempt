9072c2f41bb4926896ce5a6137b79a3acbf057e4
chrohan <chrohan007@gmail.com>
2021-10-17 07:59:34 +0530

    Ckac

---
+++ Ckac.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    int year;
+    printf("enter year\n");
+    scanf("%d", & year);
+
+        if ((year % 4) == 0)
+            printf("leap year\n");
+    else printf("not a leap year");
+}
