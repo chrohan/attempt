265fbee408e4b2320c9661c353bdf1b578ed0c69
chrohan <chrohan007@gmail.com>
2021-10-17 10:02:10 +0530

    ckag

---
+++ ckag.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the three angles of the triangle\n");
+scanf("%d%d%d",&a,&b,&c);
+
+    if((a+b+c)==180)
+       printf("triangle is valid\n");
+    else
+       printf("triangle is not valid");
+
+
+
+
+
+
+}
