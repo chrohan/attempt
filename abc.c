da1881477220e78ae86ef9d8daf4578c14160cf2
chrohan <chrohan007@gmail.com>
2021-10-17 09:24:52 +0530

    e

--- abc.c
+++ abc.c
@@ -6,9 +6,9 @@ int main()
     printf("enter a five digit number\n");
     scanf("%d", x);
     int l = x % 10;
-    int m = (x / 10) % 100;
-    int n = (x / 100) % 1000;
-    int o = (x / 1000) % 10000;
+    int m = (x / 10) % 10;
+    int n = (x / 100) % 10;
+    int o = (x / 1000) % 10;
     int p = x / 10000;
     int y = 10000 * p + 1000 * o + 100 * n + 10 * m + l;
     if (x == y)
