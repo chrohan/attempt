32db51bd8cc6f6a059ce6209367a4e13b2aa1dfb
chrohan <chrohan007@gmail.com>
2021-10-17 09:43:41 +0530

    correction in Ckac

--- Ckac.c
+++ Ckac.c
@@ -4,9 +4,23 @@ int main()
 
     int year;
     printf("enter year\n");
-    scanf("%d", & year);
+    scanf("%d", &year);
 
-        if ((year % 4) == 0)
+    if ((year % 100) == 0)
+    {
+        if ((year % 400) == 0)
+        
             printf("leap year\n");
-    else printf("not a leap year");
+            else 
+            printf("not a leap year\n");
+        
+        }
+        else
+        {
+            if ((year % 4) == 0)
+                printf("leap year\n");
+            else
+                printf("not a leap year");
+        }
+    
 }
