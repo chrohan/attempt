84d5d998c45c134fae7d295aa05ea25caae65789
chrohan <chrohan007@gmail.com>
2021-10-17 09:03:17 +0530

    Cka e

---
+++ abc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+
+    int x;
+    printf("enter a five digit number\n");
+    scanf("%d", x);
+    int l = x % 10;
+    int m = (x / 10) % 100;
+    int n = (x / 100) % 1000;
+    int o = (x / 1000) % 10000;
+    int p = x / 10000;
+    int y = 10000 * p + 1000 * o + 100 * n + 10 * m + l;
+    if (x == y)
+        printf("x and y are equal");
+    else
+        printf("x and y are not equal");
+}
