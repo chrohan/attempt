8d28987ecb48df35a496c5456ec92c7ded589fde
chrohan <chrohan007@gmail.com>
2021-10-19 11:58:34 +0530

    chapter4dkab

---
+++ Dkab.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+  
+  char a;
+  scanf("%c",&a);
+  
+  
+ if(a>=65 && a<=90)
+ printf("capital letter\n");
+
+ else if(a>=97 && a<=122)
+ printf("small case letter");
+
+ else if(a>=48 && a<=57)
+ printf("a digit or a special symbol");
+ 
+
+
+
+
+
+
+
+
+}
